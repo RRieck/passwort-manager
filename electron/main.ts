643b447ad2e6d63a68766c67b565927243e73767
Ron <rieck@landaumedia.de>
2020-06-14 14:50:33 +0200

    added Icon

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, nativeImage } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
 
@@ -6,8 +6,11 @@ let mainWindow: Electron.BrowserWindow | null;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 800,
+        title: 'password manager',
+        backgroundColor: '#3d3d3d',
+        titleBarStyle: 'hidden',
         webPreferences: {
             nodeIntegration: true,
         },
@@ -29,6 +32,9 @@ function createWindow() {
     mainWindow.on('closed', () => {
         mainWindow = null;
     })
+
+    mainWindow.setMenu(null);
+    mainWindow.setIcon('C:\\Users\\ronri\\Documents\\GitHub\\passwort-manager\\assets\\icon.png')
 }
 
 app.on('ready', createWindow);
--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
+import { Landing } from "./landing/index";
 
 const mainElement = document.createElement("div");
 document.body.appendChild(mainElement);
 
+const Base = styled.div`
+  *,
+  body,
+  html {
+    margin: 0px !important;
+    padding: 0px !important;
+  }
+`;
+
 const App = () => {
   return (
-    <div>
-      <h2>Hello From React!</h2>
-    </div>
+    <Base>
+      <Landing />
+    </Base>
   );
 };
 
